2f09d9ccd49c407f770a7dd7eab5965e5bbbdb99
Ducquanbm2 <quanitk10@gmail.com>
2026-03-24 16:11:21 +0700

    .

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello Github")
+input_str = input("Enter a list of numbers separated by space: ")
+
+numbers = [int(num) for num in input_str.split()]
+
+print("The list of numbers is:", numbers)
